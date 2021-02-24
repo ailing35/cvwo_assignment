7357d43806560247dcd3e77ad42de3d459c9c501
ailing35 <hong-ailing@hotmail.com>
2021-02-24 16:47:55 +0800

    heroku frontend commit

--- app/javascript/components/Tasks.jsx
+++ app/javascript/components/Tasks.jsx
@@ -163,7 +163,7 @@ class Tasks extends React.Component {
                 </div>
                 <div className="column">
                   <button className="btn btn-light align-middle">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="32" fill="green" className="bi bi-check2 float-right" viewBox="0 0 16 16" onClick={() => { this.deleteTask(task.id) }}>
+                    <svg xmlns="https://www.w3.org/2000/svg" width="30" height="32" fill="green" className="bi bi-check2 float-right" viewBox="0 0 16 16" onClick={() => { this.deleteTask(task.id) }}>
                       <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z" />
                     </svg>
                   </button>
@@ -171,7 +171,7 @@ class Tasks extends React.Component {
                 <div className="column">
                   {!task.editToggle
                     ? <button className="btn btn-light align-middle">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="30" height="32" fill="currentColor" className="bi bi-pen float-right" viewBox="0 0 16 16" onClick={() => this.editStart(task.id)}>
+                      <svg xmlns="https://www.w3.org/2000/svg" width="30" height="32" fill="currentColor" className="bi bi-pen float-right" viewBox="0 0 16 16" onClick={() => this.editStart(task.id)}>
                         <path d="M13.498.795l.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z" />
                       </svg>
                     </button>
@@ -179,7 +179,7 @@ class Tasks extends React.Component {
                 </div>
                 <div className="column">
                   <button className="btn btn-light align-middle">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="32" fill="red" className="bi bi-trash float-right" viewBox="0 0 16 16" onClick={() => { this.deleteTask(task.id) }}>
+                    <svg xmlns="https://www.w3.org/2000/svg" width="30" height="32" fill="red" className="bi bi-trash float-right" viewBox="0 0 16 16" onClick={() => { this.deleteTask(task.id) }}>
                       <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
                       <path fillRule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
                     </svg>
@@ -213,7 +213,7 @@ class Tasks extends React.Component {
           <main className="container">
             <div className="text-right mb-3">
               <Link to="/new_task">
-                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="black" className="bi bi-plus-square-fill" viewBox="0 0 16 16">
+                <svg xmlns="https://www.w3.org/2000/svg" width="32" height="32" fill="black" className="bi bi-plus-square-fill" viewBox="0 0 16 16">
                   <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3a.5.5 0 0 1 1 0z" />
                 </svg>
               </Link>
